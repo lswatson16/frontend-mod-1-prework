899e8b2c81d0ace4987121fb355c91d5f6490230
Lauralyn Watson <lauralynw16@gmail.com>
2021-11-02 09:37:29 -0400

    Add content

--- section1/exercises/concatenation.js
+++ section1/exercises/concatenation.js
@@ -22,8 +22,10 @@ var creatures = "unicorns";
 console.log("My zoo has " + numberOfCreatures + " " + creatures + "!")
 
 // YOU DO: What data type will be logged to the console as a result of line 22? Explain.
-// Your answer and explanation here: 
-
+// Your answer and explanation here:
+/* A string will be logged as a result of line 22. When we concatenate different data types
+like in this example we have a string and a number, it results in a type coercion.
+Javascript converts the number into a string in order to combine the two together. */
 
 // YOU DO:
 // Write code that combines the variables below into a string that
@@ -31,7 +33,7 @@ console.log("My zoo has " + numberOfCreatures + " " + creatures + "!")
 var speedy = "quick red fox";
 var slowPoke = "lazy brown dog";
 
-
+console.log("The" + speedy + " jumped over the " + slowPoke);
 // YOU DO:
 // Write code that combines the variables below into a string that
 // reads "In a predictable result, the tortoise beat the hare!"
@@ -40,16 +42,20 @@ var slowPoke = "lazy brown dog";
 slowPoke = "tortoise";
 speedy = "hare";
 
-
+console.log("In a predictable result, the " + slowPoke + " beat the " + speedy "!");
 // YOU DO:
-// Declare three variables, name/content/data type of your choice. Think carefully about what 
+// Declare three variables, name/content/data type of your choice. Think carefully about what
   // you name the variables. Remember, the goal is to be concise but descriptive (it's a hard balance!)
   // Then, log out ONE sentence that incorporates all THREE variables.
+var name = "Kat"
+var tasksForMonday = "laundry and dishes"
+var tasksForTuesday = "reading and studying"
 
+console.log(name + " has completed today's chores, " +tasksForMonday + " so she's going to get ahead on Tuesday's chores " + tasksForTuesday) ".";
 
 //-------------------
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console 
+// Did you run this file in your terminal to make sure everything printed out to the console
   // as you would expect? You should be doing this by now, and getting into the habit of it.
